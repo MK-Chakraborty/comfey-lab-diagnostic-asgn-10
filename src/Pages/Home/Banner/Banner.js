782c5f7204ef28317090cee2ba948fbeeac64cfe
MK-Chakraborty <mkchakraborty5@gmail.com>
2021-10-18 03:00:10 +0600

    footer and banner added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import { Carousel, Container } from 'react-bootstrap';
+import img1 from '../../../images/Background.png';
+import img2 from '../../../images/Background (2).png';
+import img3 from '../../../images/Background (1).png';
 
 const Banner = () => {
     return (
-        <div>
-            
-        </div>
+        <Container className='border-0 border-start border-end border-5 rounded border-primary my-5'>
+            <Carousel variant="dark" fade>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={img1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption className='text-white'>
+                        <h2>In Pursuit of Answers</h2>
+                        <h5 className='d-none d-md-block'>From diagnostics to drug development, we’ll never stop working to move health forward—every day for everyone.</h5>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={img2}
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h2>Get Ahead of Your Health</h2>
+                        <h5 className='d-none d-md-block'>Purchase your own health tests and get the answers you need to help keep your wellness on track.</h5>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={img3}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption className='fw-bold'>
+                        <h2>Kid-Friendly COVID-19 Home Collection Kit</h2>
+                        <h5 className='d-none d-md-block'>Back to school is upon us, and Pixel makes it easy to get testing for children ages 2 and up.</h5>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </Container>
     );
 };
 
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,12 +1,80 @@
 import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <Container fluid className='bg-primary'>
+        <Container fluid className='bg-primary mt-5 py-5'>
             <Container>
                 <Row>
-                    
+                    <Col xs={12} sm={12} md={6} lg={3} className="text-white text-start d-flex align-items-center pb-5">
+                        <div>
+                            <h4 className="fs-4 fst-italic"><i className="fas fa-clinic-medical"></i><span className='fw-bold'> Comfey</span> <span className='fs-5'>LAB</span> </h4>
+                            <h6><i className="far fa-copyright"></i> All Rights Reserved</h6>
+                        </div>
+                    </Col>
+                    <Col xs={12} sm={12} md={6} lg={3} className="text-white text-start pb-5">
+                        <h5>FAQs</h5>
+                        <ul style={{ listStyleType: 'none' }} className='ps-0'>
+                            <li>
+                                <NavLink to='/home'><p className='text-white'>Labs</p></NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/home'><p className='text-white'>Test Results</p></NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/home'><p className='text-white'>All Patient</p></NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/home'><p className='text-white'>All Provider</p></NavLink>
+                            </li>
+                        </ul>
+                    </Col>
+                    <Col xs={12} sm={12} md={6} lg={3} className="text-white text-start pb-5">
+                        <h5>Quick Links</h5>
+                        <ul style={{ listStyleType: 'none' }} className='ps-0'>
+                            <li>
+                                <NavLink to='/home'><p className='text-white'>Drug Development</p></NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/home'><p className='text-white'>Drug Testing</p></NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/home'><p className='text-white'>Paternity Testing</p></NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/home'><p className='text-white'>Pixel by Comfey Lab</p></NavLink>
+                            </li>
+                        </ul>
+                    </Col>
+                    <Col xs={12} sm={12} md={6} lg={3} className="text-white text-start pb-5">
+                        <h5>Contact Us</h5>
+                        <ul style={{ listStyleType: 'none' }} className='ps-0'>
+                            <li>
+                                <NavLink to='/home'><p className='text-white'>Patient</p></NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/home'><p className='text-white'>Provider</p></NavLink>
+                            </li>
+                        </ul>
+                        <ul style={{ listStyleType: 'none' }} className='d-flex justify-content-between ps-0'>
+                            <li>
+                                <NavLink to='/home'><h5 className='text-white'><i className="fab fa-facebook-f"></i></h5></NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/home'><h5 className='text-white'><i className="fab fa-twitter"></i></h5></NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/home'><h5 className='text-white'><i className="fab fa-linkedin-in"></i></h5></NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/home'><h5 className='text-white'><i className="fab fa-youtube"></i></h5></NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/home'><h5 className='text-white'><i className="fab fa-instagram"></i></h5></NavLink>
+                            </li>
+                        </ul>
+                    </Col>
                 </Row>
             </Container>
         </Container>
